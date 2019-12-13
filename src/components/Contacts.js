c6b0c17277dbde5e0a45ca450112f6ec20004467
Hannes <h.geissler@schille.com>
2019-12-13 11:52:44 +0100

    Contacts.js ready for iterating through contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import SingleContact from './SingleContact';
+import AddContacts from './AddContacts';
 
 export default class Contacts extends Component {
   constructor(props) {
@@ -8,9 +10,25 @@ export default class Contacts extends Component {
     }
   }
 
+  // fetch Data from server before component renders
+  componentDidMount() {
+    fetch('http://localhost:8080/api/contacts')
+    .then(response => response.json())
+    .then(data => this.setState({contacts: data}))
+  }
+
   render() {
     return (
       <div>
+        <div className="row">
+          <AddContacts />
+        </div>
+        <div className="row">
+          {/* iterate through contacts-array */}
+          { this.state.contacts.map((item) => {
+            <SingleContact key={item.id} item={item}/>
+          })}
+        </div>
         <p>Hello</p>
       </div>
     )
